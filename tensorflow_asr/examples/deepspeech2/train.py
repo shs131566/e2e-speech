8d154714b48909ecab06b27f503ec87a455740c9
shs131566 <philip1993@gmail.com>
2022-02-23 15:59:20 +0900

    add deepspeech2 config file

---
+++ tensorflow_asr/examples/deepspeech2/train.py
@@ -0,0 +1,3 @@
+import os
+
+DEFAULT_YAML = os.path.join(os.path.abspath(os.path.dirname(__file__)), "config.yml")
--- tensorflow_asr/featurizers/speech_featurizers.py
+++ tensorflow_asr/featurizers/speech_featurizers.py
@@ -13,8 +13,21 @@ def load_and_convert_to_wav(path: str) -> tf.Tensor:
     return tf.audio.encode_wav(tf.expand_dims(wave, axis=1), sample_rate=rate)
 
 def read_raw_audio(audio: Union[str, bytes, np.ndarray], sample_rate=16000) -> np.ndarray:
-    # audio 파일 경로가 입렵된 경우
     if isinstance(audio, str):
-        wave, _ = librosa.load(os.path.expanduser(audio), sr=None, mono=True)
+        wave, _ = librosa.load(os.path.expanduser(audio), sr=sample_rate, mono=True)
     elif isinstance(audio, bytes):
         wave, sr = sf.read(io.BytesIO(audio))
+        if wave.ndim > 1:
+            wave = np.mean(wave, axis=-1)
+        wave = np.asfortranarray(wave)
+        if sr != sample_rate:
+            wave = librosa.resample(wave, sr, sample_rate)
+    elif isinstance(audio, np.ndarray):
+        if audio.ndim > 1:
+            ValueError("input audio must be single channel")
+        return audio
+    else:
+        raise ValueError("input audio must be either a path or bytes")
+    return audio
+
+def tf_read_raw_audio(audio: tf.Tensor, sample_rate=16000) -> tf.Tensor:
